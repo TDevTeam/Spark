134acb5a550522c8ecfb1ddca6840b94c996d2a7
Ananmay 125 <ananmay125@gmail.com>
2024-05-04 22:43:45 +0530

    first upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express'); 
+
+const app = express(); 
+const PORT = 3000; 
+
+app.use(express.static("public"));
+
+app.get('/', (req, res)=>{ 
+	res.status(200); 
+	res.sendFile(__dirname + "/index.html");
+}); 
+
+app.get('/home', (req, res)=>{ 
+	res.status(200); 
+	res.sendFile(__dirname + "/index.html");
+}); 
+
+app.get('/login', (req, res)=>{ 
+	res.status(200); 
+	res.sendFile(__dirname + "/login.html");
+}); 
+
+app.get('/register', (req, res)=>{ 
+	res.status(200); 
+	res.sendFile(__dirname + "/register.html");
+}); 
+
+app.listen(PORT, (error) =>{ 
+	if(!error) 
+		console.log("Server is Successfully Running,and App is listening on port "+ PORT);
+	else
+		console.log("Error occurred, server can't start", error); 
+	} 
+); 
