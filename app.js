65f50a3c7c44facdb05f62ef4d8029d731db2c6c
Ananmay 125 <ananmay125@gmail.com>
2024-05-06 20:41:49 +0530

    small update

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 const express = require('express'); 
-
+  
 const app = express(); 
-const PORT = 3000; 
+const PORT = 3000;
 
 app.use(express.static("public"));
-
+  
 app.get('/', (req, res)=>{ 
-	res.status(200); 
-	res.sendFile(__dirname + "/index.html");
-}); 
+  res.status(200); 
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.get('/home', (req, res)=>{ 
-	res.status(200); 
-	res.sendFile(__dirname + "/index.html");
-}); 
+  res.status(200); 
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.get('/login', (req, res)=>{ 
-	res.status(200); 
-	res.sendFile(__dirname + "/login.html");
-}); 
+  res.status(200); 
+  res.sendFile(__dirname + "/login.html");
+});
 
 app.get('/register', (req, res)=>{ 
-	res.status(200); 
-	res.sendFile(__dirname + "/register.html");
-}); 
+  res.status(200); 
+  res.sendFile(__dirname + "/register.html");
+});
 
+app.get('/post', (req, res)=>{ 
+  res.status(200); 
+  res.sendFile(__dirname + "/post.html");
+});
+  
 app.listen(PORT, (error) =>{ 
-	if(!error) 
-		console.log("Server is Successfully Running,and App is listening on port "+ PORT);
-	else
-		console.log("Error occurred, server can't start", error); 
-	} 
-); 
+  if(!error) 
+    console.log("Server is Successfully Running,and App is listening on port "+ PORT);
+  else
+    console.log("Error occurred, server can't start", error); 
+  } 
+);
